7ec35f6be5cff8574a2a1cdc1422f2025cc397ae
Luis Henrique <luishpmenezes@hotmail.com>
2025-04-08 21:09:20 -0300

    att Pessoa

--- src/main/java/com/example/curriculosb/Model/Pessoa.java
+++ src/main/java/com/example/curriculosb/Model/Pessoa.java
@@ -1,4 +1,18 @@
 package com.example.curriculosb.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Pessoa {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String profissao;
+    private String resumo;
+
+
 }
